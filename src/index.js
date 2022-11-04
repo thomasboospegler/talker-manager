35938b17b825065e187436b5ad447c72d5eda1b8
Thomas Boos Pegler <tboospegler@gmail.com>
2022-11-04 15:01:06 -0400

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { getAllTalkers } = require('./utils/handleTalkers');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,4 +15,10 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+//Talkers
+app.get('/talker', async (req, res) => {
+  const talkers = await getAllTalkers();
+  res.status(200).send(talkers)
 });
---
+++ src/utils/handleTalkers.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const talkerPath = path.resolve(__dirname, '..', 'talker.json');
+
+const getAllTalkers = async () => {
+  const response = await fs.readFile(talkerPath, 'utf-8');
+  const talkers = JSON.parse(response);
+  return talkers
+};
+
+module.exports = {
+  getAllTalkers,
+};
